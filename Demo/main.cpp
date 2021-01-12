70fd6784166d57d6c4a289754477dfc78478faef
Tai-Wong <jp092741@gmail.com>
2021-01-12 19:35:32 +0800

    Initial Commit

---
+++ Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Demo
+//
+//  Created by Huang Tai on 2021/1/12.
+//  Copyright © 2021 Huang Tai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
